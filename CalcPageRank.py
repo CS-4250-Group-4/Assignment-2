e7734ce227844f27cffe83ab2f6f81ae30826620
Krys <21113012+Krystade@users.noreply.github.com>
2022-04-25 17:34:54 -0700

    calculates page rank from collected data

--- CalcPageRank.py
+++ CalcPageRank.py
@@ -1,4 +1,7 @@
 def main():
+    global pageDict
+    global linkDict
+    
     #domains
     domain = "cpp"
     #domain = "ameblo"
@@ -16,7 +19,7 @@ def main():
             #Remove first { from array
             #Delete last } from array
             if line:
-                vals = line.replace("}","").replace("{","").replace("\'","").replace(" ","")
+                vals = line.replace("[","").replace("]","").replace("\'","").replace(" ","")
                 vals = vals.split(",")
                 for val in vals:
                     inlinks.append(val)
@@ -34,6 +37,12 @@ def main():
 
     #Sum up the in-link prvalue/ in-link # of outlinks
     for i in range(10):
+        maxRank = 0
+        maxRank2 = 0
+        maxRank3 = 0
+        maxPage = ""
+        maxPage2 = ""
+        maxPage3 = ""
         for page in pageDict:
             inlinkSum = 0
             try:
@@ -44,11 +53,50 @@ def main():
                         inlinkSum += pageDict[inlink][1]/int(pageDict[inlink][0])
                 pageDict[page] = (pageDict[page][0], inlinkSum)
             except(KeyError):
-                print("not found in linkDict" + page)
+                #print("not found in linkDict" + page)
+                pass
         print("CYCLE " + str(i) + " COMPLETE")
         for page in pageDict:
             #print(page + "/t" + str(pageDict[page]))
-            print(pageDict[page][1])
+            #print(pageDict[page][1])
+            pass
+            if (pageDict[page][1] > maxRank):
+                maxRank = pageDict[page][1]
+                maxPage = page
+            elif (pageDict[page][1] > maxRank2):
+                maxRank2 = pageDict[page][1]
+                maxPage2 = page
+            elif (pageDict[page][1] > maxRank3):
+                maxRank3 = pageDict[page][1]
+                maxPage3 = page
+        print(maxPage + ": " + str(maxRank))
+        print(len(linkDict[maxPage]))
+        print(maxPage2 + ": " + str(maxRank2))
+        print(len(linkDict[maxPage2]))
+        print(maxPage3 + ": " + str(maxRank3))
+        print(len(linkDict[maxPage3]))
 
+        dictToCsv(domain + "_pageRank")
+        
+def dictToCsv(filename):#, fields):
+    global pageDict
+    global linkDict
+    #pageDict = {} #Page, # of outlinks
+    #linkDict = {} # key Page, list of pages linking to key page
+    filename += ".csv"
+
+    # writing to csv if its just a regular dict without an array
+    with open(filename, 'w') as csvfile:
+        csvfile.write("Page,Inlinks,Outlinks,Page Rank\n")
+        for entry in pageDict:
+            entry = str(entry)
+            csvfile.write(entry + ",")
+            try:
+                csvfile.write(str(len(linkDict[entry])) + ",")
+            except KeyError:
+                csvfile.write("0,")
+            csvfile.write(str(pageDict[entry]).replace("(","").replace(")","").replace("\'","").replace(" ","") + "\n")
+            
 if __name__ == "__main__":
+    
     main()
